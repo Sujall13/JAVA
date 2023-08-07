48544a349aab924c847ca7ae823717aeac961442
Sujall13 <141057003+Sujall13@users.noreply.github.com>
2023-08-07 22:06:20 +0530

    Create if.java

---
+++ if.java
@@ -0,0 +1,10 @@
+class ifdemo {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        if( a+b > 10) {
+            System.out.println("a + b is greater than 10");
+        }
+    }
+    
+}
